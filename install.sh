7e637f33bba0c9b4084f0506efb840d62aba64eb
VishnuSanal <t.v.s10123@gmail.com>
2023-05-25 13:49:09 +0530

    update install script

--- install.sh
+++ install.sh
@@ -3,9 +3,14 @@
 mkdir -p ~/.local/share/fonts
 cp assets/* ~/.local/share/fonts
 
-mv ~/.config/conky/ ~/.config/conky-backup
+if [ -d ~/.config/conky/ ]; then
+	mv ~/.config/conky/ ~/.config/conky-backup
+	echo "Copied ~/.config/conky to ~/.config/conky-backup"
+fi
 
 mkdir -p ~/.config/conky
 
 cp conky.conf ~/.config/conky/
 cp myluascript.lua ~/.config/conky/
+
+echo "Installation completed!" 
